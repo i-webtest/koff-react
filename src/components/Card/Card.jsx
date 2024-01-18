e0329283096a3c0d7ae47f6fb93e49f8b9118d0b
i-webtest <stasbass80@gmail.com>
2024-01-18 16:33:57 +0300

    Completed lesson 4 and lesson 05

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,85 +1,74 @@
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { Navigation, Thumbs } from 'swiper/modules';
-import 'swiper/css';
 import s from './Card.module.scss';
 import { Container } from '../../views/Container/Container';
-import { useState } from 'react';
+import { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 
-export const Card = () => {
-  const [mainSwiper, setMainSwiper] = useState(null);
-  const [thumbsSwiper, setThumbsSwiper] = useState(null);
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchProduct } from '../../store/product/productSlice';
+import { Slider } from '../Slider/Slider';
 
+export const Card = () => {
   const { productId } = useParams();
-  console.log('productId: ', productId);
+  const dispatch = useDispatch();
+
+  const { data, loading, error } = useSelector((state) => state.product);
+
+  useEffect(() => {
+    dispatch(fetchProduct(productId));
+  }, [dispatch, productId]);
+
+  if (loading) return <div>Загрузка...</div>;
+  if (error) return <div>Ошибка: {error}</div>;
 
   return (
     <section className={s.card}>
       <Container className={s.container}>
         <h2 className={s.title}>Кресло с подлокотниками</h2>
 
-        <div className={s.picture}>
-          <div className={s.sliderMain}>
-            <Swiper
-              onSwiper={setMainSwiper}
-              modules={[Thumbs]}
-              thumbs={{ swiper: thumbsSwiper }}
-              spaceBetween={10}
-            >
-              <SwiperSlide>
-                <img src='/img/chair.jpg' alt='Кресло с подлокотниками' />
-              </SwiperSlide>
-              <SwiperSlide>
-                <img src='/img/chair.jpg' alt='Кресло с подлокотниками' />
-              </SwiperSlide>
-              <SwiperSlide>
-                <img src='/img/chair.jpg' alt='Кресло с подлокотниками' />
-              </SwiperSlide>
-              <SwiperSlide>
-                <img src='/img/chair.jpg' alt='Кресло с подлокотниками' />
-              </SwiperSlide>
-              <SwiperSlide>
-                <img src='/img/chair.jpg' alt='Кресло с подлокотниками' />
-              </SwiperSlide>
-            </Swiper>
-
-            <button onClick={() => mainSwiper.slidePrev()}>prev</button>
-            <button onClick={() => mainSwiper.slideNext()}>next</button>
-          </div>
-
-          <div className={s.sliderThumbnails}>
-            <Swiper
-              onSwiper={setThumbsSwiper}
-              modules={[Thumbs]}
-              watchSlidesProgress
-              spaceBetween={14}
-              slidesPerView={4}
-            >
-              <SwiperSlide>
-                <img src='/img/chair.jpg' alt='Кресло с подлокотниками' />
-              </SwiperSlide>
-              <SwiperSlide>
-                <img src='/img/chair.jpg' alt='Кресло с подлокотниками' />
-              </SwiperSlide>
-              <SwiperSlide>
-                <img src='/img/chair.jpg' alt='Кресло с подлокотниками' />
-              </SwiperSlide>
-              <SwiperSlide>
-                <img src='/img/chair.jpg' alt='Кресло с подлокотниками' />
-              </SwiperSlide>
-              <SwiperSlide>
-                <img src='/img/chair.jpg' alt='Кресло с подлокотниками' />
-              </SwiperSlide>
-            </Swiper>
-          </div>
-        </div>
+        <Slider data={data} />
 
         <div className={s.info}>
           {/* <p className={s.price}>{'5000'.toLocalString()}&nbsp;₽</p> */}
           <p className={s.article}>арт. 84348945757</p>
 
-          <div className={s.characteristics}>
-            <h3 className={s.subtitle}>Общие характеристики</h3>
+          <div>
+            <h3 className={s.characteristicsTitle}>Общие характеристики</h3>
+
+            <table className={s.table}>
+              <tbody>
+                {data.characteristics.map((item, i) => (
+                  <tr className={s.row} key={i}>
+                    <td className={s.field}>{item[0]}</td>
+                    <td className={s.value}>{item[1]}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+
+            <div className={s.btns}>
+              <button className={s.btn} type='button'>
+                В корзину
+              </button>
+
+              <button className={s.like} type='button'>
+                <svg
+                  xmlns='http://www.w3.org/2000/svg'
+                  width='16'
+                  height='16'
+                  viewBox='0 0 16 16'
+                  fill='none'
+                >
+                  <path
+                    // eslint-disable-next-line max-len
+                    d='M8.4135 13.8733C8.18683 13.9533 7.8135 13.9533 7.58683 13.8733C5.6535 13.2133 1.3335 10.46 1.3335 5.79332C1.3335 3.73332 2.9935 2.06665 5.04016 2.06665C6.2535 2.06665 7.32683 2.65332 8.00016 3.55998C8.6735 2.65332 9.7535 2.06665 10.9602 2.06665C13.0068 2.06665 14.6668 3.73332 14.6668 5.79332C14.6668 10.46 10.3468 13.2133 8.4135 13.8733Z'
+                    fill='white'
+                    stroke='currentColor'
+                    strokeLinecap='round'
+                    strokeLinejoin='round'
+                  />
+                </svg>
+              </button>
+            </div>
           </div>
         </div>
       </Container>
--- src/store/store.js
+++ src/store/store.js
@@ -2,11 +2,16 @@ import { configureStore } from '@reduxjs/toolkit';
 import authReducer from './auth/authSlice';
 import categoriesReducer from './categories/categoriesSlice';
 import productsReducer from './products/productsSlice';
+import productReducer from './product/productSlice';
+import { apiTokenErrorMiddleware } from './middleware';
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     categories: categoriesReducer,
     products: productsReducer,
+    product: productReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(apiTokenErrorMiddleware),
 });
